5b7590191b71de95914fe9ab592bde41db35ad8c
Mihir.Kulkarni <87798563+Mihir11Kulkarni@users.noreply.github.com>
2023-10-18 15:40:45 +0530

    main1.tf
    For Azure

---
+++ main1.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  environment     = "public"
+  subscription_id = var.azure-subscription-id
+  client_id       = var.azure-client-id
+  client_secret   = var.azure-client-secret
+  tenant_id       = var.azure-tenant-id
+}
